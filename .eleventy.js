8e72ba724da4afadb6f11874ace266cd95a51c68
Yashasva Paras <theboywonder@192.168.1.13>
2023-04-09 16:03:43 +0530

    add css & build config

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const markdownIt = require('markdown-it')
 module.exports = function(eleventyConfig) {
-    
+    eleventyConfig.addPassthroughCopy("src/assets/css/main.css");
+    eleventyConfig.amendLibrary("md", mdLib => mdLib.use(markdownIt));
 }
